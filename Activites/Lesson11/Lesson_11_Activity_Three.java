075f1b0b35141f2623c37c5224378f46ed479e07
Zach <zakkyzakkba@gmail.com>
2016-10-18 15:19:40 -0400

    Update Lesson_11_Activity_Three.java

--- Activites/Lesson11/Lesson_11_Activity_Three.java
+++ Activites/Lesson11/Lesson_11_Activity_Three.java
@@ -10,14 +10,20 @@ public class Lesson_11_Activity_Three {
 	
 	public static void main(String[] args)
 	{
+		// Create scanner object so we can input data from the console
 		Scanner scan = new Scanner(System.in);
 		
+		// Ask user for two integers
+		System.out.println("Please input two integers");
 		int num1 = scan.nextInt();
 		int num2 = scan.nextInt();
 		
+		// Test to see if the two integers are the same
+		// IF the two numbers are equal, then print yes
 		if(num1 == num2)
 			System.out.println("YES");
 		
+		// Disregard, closes scanner to lower memory usage
 		scan.close();
 	}
 }
