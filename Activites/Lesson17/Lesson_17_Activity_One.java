41557a1936069c0c9d8c4a1f8102d02f06952e5e
Zach <zakkyzakkba@gmail.com>
2016-11-15 19:08:28 -0500

    Fixed formatting & added comments

--- Activites/Lesson17/Lesson_17_Activity_One.java
+++ Activites/Lesson17/Lesson_17_Activity_One.java
@@ -3,25 +3,31 @@ import java.util.Scanner;
 public class Lesson_17_Activity_One
 {
 	 public static void main(String[] args) {
-	        Scanner scan = new Scanner(System.in);
-		    
-		    int score = 0;
-		    double cumlativeScores = 0;
-		    int numberOfTests = 0;
-		    while(score != -1)
-		    {
-		    	
-		    	
-		        score = scan.nextInt();
-		        cumlativeScores += (double) score;
-		        numberOfTests++;
-		        	        
-		    }
-		    
-	        cumlativeScores += 1;
-		    numberOfTests -= 1;
-		    
-		    System.out.println("The average is: " + (cumlativeScores / numberOfTests));
-		    scan.close();
-	    }
+		Scanner scan = new Scanner(System.in);
+		 
+		// Create varibla to track the score the user is inputing
+		int score = 0;
+		// Keep track of all test scores combined for calculating the average
+		double cumulativeScores = 0;
+		// Keep track of the number of tests submitted.
+		int numberOfTests = 0;
+		while (score != -1)
+		{
+			// Get the score that was input
+			score = scan.nextInt();
+			// Add the score to the combined score amount
+			cumulativeScores += (double) score;
+			// Add one to the number of tests
+			numberOfTests++;
+
+		}
+		
+		// Add one to the test score because it ran through the while loop when 1 was == 0, this way we get the correct cumulative
+		cumulativeScores += 1;
+		// Remove a test because it ran through the while loop and added 1
+		numberOfTests--;
+
+		System.out.println("The average is: " + (cumlativeScores / numberOfTests));
+		scan.close();
+	}
 }
