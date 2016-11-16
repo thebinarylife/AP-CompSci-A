fa3372837e304544fc62ad34cbad6e36a1c8b220
Zach <zakkyzakkba@gmail.com>
2016-11-16 00:15:43 -0500

    Added comments

--- Activites/Lesson17/Lesson_17_Activity_Two.java
+++ Activites/Lesson17/Lesson_17_Activity_Two.java
@@ -7,15 +7,20 @@ public class Lesson_17_Activity_Two
 	{
 		Scanner scan = new Scanner(System.in);
 		
+		// Store both numbers
 		System.out.println("Enter two numbers:");
 		int x = scan.nextInt();
 		int y = scan.nextInt();
+		
+		// While x is not greater than y
 		while(!(x > y))
 		{
+			// Check to see if the number is even by checking to see if it's divisible by two
 			if(x % 2 == 0)
 			{
 				System.out.print(x + " ");
 			}
+			// Up the number by one
 			x++;
 		}
 		
