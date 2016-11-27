9ba381766675c421018748e340138ae018802f86
Zach <zakkyzakkba@gmail.com>
2016-11-27 16:37:22 -0500

    Create Lesson_29_Activity_One.java

---
+++ Activites/Lesson29/Lesson_29_Activity_One.java
@@ -0,0 +1,32 @@
+/*
+ * Lesson 29 Coding Activity 1
+ * A student wants an algorithm to find the hardest spelling
+ * word in a list of vocabulary. They define hardest by the longest word.
+ * Write the code to find the longest word stored in an array of Strings called list.
+ * If several words have the same length it should print the first word 
+ * in list with the longest length.
+ * For example, if the following list were declared:
+ * 
+ *     String list [] = {"high", "every", "nearing", "checking", "food ", 
+ *     "stand", "value",  "best", "energy", "add", "grand", "notation",
+ *     "abducted", "food ", "stand"};
+ * 
+ * It would print:
+ *     checking 
+ */
+
+class Lesson_29_Activity_One {
+	public static String[] list = {};
+	
+	public static void main(String[] args)
+	{
+		String s = null;
+		for(int i = 0; i < list.length; i++)
+		{
+			if(s == null || list[i].length() > s.length())
+				s = list[i];
+		}
+		
+		System.out.println(s);
+	}
+}
