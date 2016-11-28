f1fc6624c807ec0f9cd1dd9346d1e57c0f4df94e
Zach <zakkyzakkba@gmail.com>
2016-11-28 17:54:01 -0500

    Update Lesson_29_Activity_Two.java

--- Activites/Lesson29/Lesson_29_Activity_Two.java
+++ Activites/Lesson29/Lesson_29_Activity_Two.java
@@ -21,14 +21,21 @@ class Lesson_29_Activity_Two {
 	
 	public static void main(String[] args)
 	{
+		// Start at 0 since the array starts and zero and end when the variable is one less than the length
 		for(int i = 0; i < list.length; i++)
 		{
+			// Create a new variable with no text to store the new word
 			String s = "";
+			// Start at the length of the array - 1 because we know that this will be the last element of the array
+			// Wait until the array hits zero, include zero because we know that the array starts at zero
+			// Through every loop decrease it by one
 			for(int ii = list[i].length() - 1; ii > -1; ii--)
 			{
+				// add the character to the string variable
 				s += list[i].charAt(ii);
 			}
 			
+			// Print the new string
 			System.out.println(s);
 		}
 	}
