8a8751ecfc7f27738bb680bc47617cfe475f68b2
Zach <zakkyzakkba@gmail.com>
2016-11-28 17:49:36 -0500

    Update Lesson_29_Activity_One.java

--- Activites/Lesson29/Lesson_29_Activity_One.java
+++ Activites/Lesson29/Lesson_29_Activity_One.java
@@ -20,13 +20,18 @@ class Lesson_29_Activity_One {
 	
 	public static void main(String[] args)
 	{
+		// Create a String that will hold the largest string
 		String s = null;
+		// Starting at 0, since an array starts at 0, increment it up until it is the == the length of the array
 		for(int i = 0; i < list.length; i++)
 		{
+			// if the string is still = null, or the length of the string is the length of the value in the array
+			// If it is longer, save it in the String variable
 			if(s == null || list[i].length() > s.length())
 				s = list[i];
 		}
 		
+		// Print the final String value
 		System.out.println(s);
 	}
 }
