56f13ca4180c128ec5c520b50e2fd98f57e9b185
Zach <zakkyzakkba@gmail.com>
2016-11-28 17:34:28 -0500

    Update Lesson_24_Activity_Two.java

--- Activites/Lesson24/Lesson_24_Activity_Two.java
+++ Activites/Lesson24/Lesson_24_Activity_Two.java
@@ -2,9 +2,12 @@ public class Lesson_24_Activity_Two {
 	
 	public static void main(String[] args)
 	{
+		// Start at 1 and incrememnt all the way up to 50
 		for(int i = 1; i < 51; i++)
 		{
+			// If the number is divisible by 2
 			if(i % 2 == 0)
+				// Print it
 				System.out.println(i);
 		}
 	}
