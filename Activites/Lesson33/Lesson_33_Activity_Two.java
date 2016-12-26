ba6a259881aef68e456fb6f77c5a44cf4d6a7270
Zach <zakkyzakkba@gmail.com>
2016-12-26 12:02:28 -0500

    Update Lesson_33_Activity_Two.java

--- Activites/Lesson33/Lesson_33_Activity_Two.java
+++ Activites/Lesson33/Lesson_33_Activity_Two.java
@@ -1,1 +1,12 @@
+public class Lesson_33_Activity_Two {
 
+    public static void randomize(int[] array)
+    {
+        for(int i = 0; i < array.length; i++)
+        {
+            int min = 10;
+            int max = 100;
+            array[i] = (int) (Math.random() * (max - min) + min);
+        }
+    }
+}
