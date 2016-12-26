3fe66a0521a9793738bc7ecdb195ba52b91a1c8b
Zach <zakkyzakkba@gmail.com>
2016-12-26 12:19:49 -0500

    Update Activity_33_Lesson_One.java

--- Activites/Lesson33/Activity_33_Lesson_One.java
+++ Activites/Lesson33/Activity_33_Lesson_One.java
@@ -1,12 +1,13 @@
 public class Lesson_33_Activity_One {
 
+    // Create the method with a String array parameter
     public static void upper(String[] array)
     {
+        // Iterate through each element of the array
         for(int i = 0; i < array.length; i++)
         {
+            // Set the element to uperace
             array[i] = array[i].toUpperCase();
         }
-        
-        
     }
 }
