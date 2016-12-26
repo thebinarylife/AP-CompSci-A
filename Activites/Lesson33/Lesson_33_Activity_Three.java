3df50b175642beb4aa70b46cab5cdfe09b6db558
Zach <zakkyzakkba@gmail.com>
2016-12-26 12:27:36 -0500

    Create Lesson_33_Activity_Three.java

---
+++ Activites/Lesson33/Lesson_33_Activity_Three.java
@@ -0,0 +1,10 @@
+public class Lesson_33_Activity_Three {
+
+    public static void printit(int[] array)
+    {
+        for(int i = 0; i < array.length; i++)
+        {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
