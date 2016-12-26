ca1e93c1d18dda0fc22f4d98747a240ce37d1f74
Zach <zakkyzakkba@gmail.com>
2016-12-26 12:29:41 -0500

    Update Lesson_33_Activity_Three.java

--- Activites/Lesson33/Lesson_33_Activity_Three.java
+++ Activites/Lesson33/Lesson_33_Activity_Three.java
@@ -1,9 +1,12 @@
 public class Lesson_33_Activity_Three {
 
+    // Create method printit() with an integer array parameter
     public static void printit(int[] array)
     {
+        // Iterate through the array
         for(int i = 0; i < array.length; i++)
         {
+            // Print each element with a space after
             System.out.print(array[i] + " ");
         }
     }
