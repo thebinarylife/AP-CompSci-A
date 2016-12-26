222a8caaacb7a59f492bb3fc0275e16c1a0a9458
Zach <zakkyzakkba@gmail.com>
2016-12-26 12:06:28 -0500

    Update Lesson_33_Activity_Two.java

--- Activites/Lesson33/Lesson_33_Activity_Two.java
+++ Activites/Lesson33/Lesson_33_Activity_Two.java
@@ -1,11 +1,19 @@
 public class Lesson_33_Activity_Two {
-
+    
+    // Create the method with an Integer array parameter
     public static void randomize(int[] array)
     {
+        // Create a for loop so that we can iterate through each element of the array
         for(int i = 0; i < array.length; i++)
         {
+            // We know that the minimum value is 10
             int min = 10;
+            // We know that the max will be 100, we add one to it so that it includes 100
             int max = 100;
+            
+            // Set element i in the Array to the random decimal times the range of the two numbers will give us a number in between
+            // Add the minimum value so that it is between 100 and 99
+            // Cast the number to an integer so that the element is an int
             array[i] = (int) (Math.random() * (max - min) + min);
         }
     }
