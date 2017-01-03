4adc155cf83306305d896c1e15f23764a9bb5194
Zach <zakkyzakkba@gmail.com>
2017-01-03 10:14:42 -0500

    Create temp.java

---
+++ Activites/temp.java
@@ -0,0 +1,9 @@
+public class Lesson_32_Activity_One {
+
+    public static String monthName(int monthNum)
+    {
+        String[] month = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        
+        System.out.println(month[monthNum-1]);
+    }
+}
