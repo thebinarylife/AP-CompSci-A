99b171610f5d6b6707c284235852c62c1a13a52e
Zach <zakkyzakkba@gmail.com>
2017-01-04 09:59:46 -0500

    Update and rename Activites/temp.java to Activites/Lesson32/Lesson_32_Activity_One.java

--- Activites/Lesson32/Lesson_32_Activity_One.java
+++ Activites/Lesson32/Lesson_32_Activity_One.java
@@ -1,9 +1,12 @@
 public class Lesson_32_Activity_One {
 
+    // Create the desired method with an input of an integer that corresponds with a month
     public static String monthName(int monthNum)
     {
+        // Create a string of all the months in numerical order
         String[] month = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
         
+        // Find the month using the number inputted minus 1 (because arrays start at 0)
         System.out.println(month[monthNum-1]);
     }
 }
