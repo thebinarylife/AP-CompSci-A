2e853944a813c71437bdb36a648289d900685cc9
Zach <zakkyzakkba@gmail.com>
2017-02-07 10:11:21 -0500

    Update Main.java

--- Activites/Unit5-Lesson1/Main.java
+++ Activites/Unit5-Lesson1/Main.java
@@ -7,27 +7,27 @@ public class Main {
 	{
 		Scanner scan = new Scanner (System.in);
 		
-        System.out.println("Please input words, enter STOP to stop the loop.");
-        
-        ArrayList <String> list = new ArrayList<>();
-        String s = null;
-        while (s == null || !s.equals("STOP"))
-        {
-            s = scan.nextLine();
-            list.add(s);
-        } 
-        
-        list.remove(list.size() - 1);
-        
-        System.out.println(list.size());
-        System.out.println(list);
-        if (list.size() > 2)
-        {
-            list.remove(0);
-            list.remove(list.size()-1);
-            System.out.println(list);
-        }
-        
-        scan.close();
-    }
+		System.out.println("Please input words, enter STOP to stop the loop.");
+
+		ArrayList <String> list = new ArrayList<>();
+		String s = null;
+		while (s == null || !s.equals("STOP"))
+		{
+		    s = scan.nextLine();
+		    list.add(s);
+		} 
+
+		list.remove(list.size() - 1);
+
+		System.out.println(list.size());
+		System.out.println(list);
+		if (list.size() > 2)
+		{
+		    list.remove(0);
+		    list.remove(list.size()-1);
+		    System.out.println(list);
+		}
+
+		scan.close();
+	}
 }
