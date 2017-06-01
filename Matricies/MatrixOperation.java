1e4afd909e8cf556e6e93d6f8e56f6a887ae55bd
Zach <zakkyzakkba@gmail.com>
2017-06-01 18:31:43 -0400

    Update MatrixOperation.java

--- Matricies/MatrixOperation.java
+++ Matricies/MatrixOperation.java
@@ -1,4 +1,22 @@
 public abstract class MatrixOperation
 {
-
+	protected Matrix matrix;
+	
+	public MatrixOperation(Matrix matrix)
+	{
+		this.matrix = matrix;
+	}
+	
+	public Matrix getMatrix()
+	{
+		return matrix; 
+	}
+	
+	public void setMatrix(Matrix matrix)
+	{
+		this.matrix = matrix;
+	}
+	
+	abstract Matrix runOperation();
+	
 }
