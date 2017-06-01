395845488176b6688de13dc044cb1432678b3acb
Zach <zakkyzakkba@gmail.com>
2017-06-01 10:59:30 -0400

    Update Matrix.java

--- Matricies/Matrix.java
+++ Matricies/Matrix.java
@@ -1,1 +1,27 @@
+public class Matrix 
+{
+  private int[][] matrix;
+  private int rows;
+  private int cols;
+  
+  public Matrix(int rows, int cols, int[][] matrix)
+  {
+    this.matrix = matrix;
+    this.rows = rows;
+    this.cols = cols;
+  }
+  
+  
+  public Matrix(int rows, int cols)
+  {
+    int[][] matrix = new int[rows][cols];
+    for(int x = 0; x < rows - 1; x ++)
+    {
+      for(int y = 0; y < cols - 1; y++)
+      {
+        matrix[x][y] = 0;
+      }
+    }
+  }
 
+}
